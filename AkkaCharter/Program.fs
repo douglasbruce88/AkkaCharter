79f75f0b7ff7e73f4c6eaaddd5476e8be4931872
Doug Bruce <douglasbruce88@gmail.comn>
2016-08-22 13:57:26 +0100

    UI work (basic WinForm)

--- AkkaCharter/Program.fs
+++ AkkaCharter/Program.fs
@@ -1,19 +1,11 @@
-﻿open AkkaCharter.Data.Yahoo
-open FSharp.Charting
+﻿module Program
+
 open System
 open System.Windows.Forms
+open AkkaCharter
 
-let createChart startDate endDate = 
-    [ for row in getStockPrices "MSFT" startDate endDate do
-          yield row.Date, row.High, row.Low, row.Open, row.Close ]
-    |> Chart.Candlestick
-    |> Chart.WithArea.AxisY(Minimum = 30.0, Maximum = 52.0)
+Application.EnableVisualStyles ()
+Application.SetCompatibleTextRenderingDefault false
 
-[<EntryPoint>]
-[<STAThread>]
-let main _ = 
-    Application.EnableVisualStyles()
-    Application.SetCompatibleTextRenderingDefault false
-    let chart = createChart (new DateTime(2014, 01, 01)) (new DateTime(2015, 01, 01))
-    Application.Run(chart.ShowChart())
-    0
+[<STAThread>]    
+do Application.Run (Form.load())

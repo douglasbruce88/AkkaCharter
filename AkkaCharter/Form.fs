320178d462c26415c03cc85d59fe5a58935838fd
Doug Bruce <douglasbruce88@gmail.comn>
2016-08-30 13:46:31 +0100

    ListBox Size

--- AkkaCharter/Form.fs
+++ AkkaCharter/Form.fs
@@ -13,7 +13,7 @@ module Form =
     let btnRunTasks = new Button(Name = "btnRunTasks", Text = "Run using Tasks", Location = Point(20, 300), Size = Size(110, 41), TabIndex = 1, UseVisualStyleBackColor = true)
 
     let form = new Form(Name = "Main", Visible = true, Text = "Tickers", AutoScaleDimensions = SizeF(6.F, 13.F), AutoScaleMode = AutoScaleMode.Font, ClientSize = Size(1000, 600))
-    let listBox = new ListBox(Location = new Point(20, 30), FormattingEnabled = true, SelectionMode = SelectionMode.MultiExtended)
+    let listBox = new ListBox(Location = new Point(20, 30), FormattingEnabled = true, SelectionMode = SelectionMode.MultiExtended, Height = 150)
     let tickerPanel = new Panel(Location = new Point(0, 0), Size = new Size(800, 600), BorderStyle = BorderStyle.Fixed3D, Dock = DockStyle.Fill)
     let controlPanel = new Panel(Location = new Point(800, 0), Size = new Size(200, 600), BorderStyle = BorderStyle.Fixed3D, Dock = DockStyle.Right)
     let items = [ "AAPL"; "ADR"; "AMZN"; "GOOG"; "HPQ"; "IBM"; "MSFT"; "NOK"; "TWTR"; "TYO" ]
